3866cb60cc9e58390210cfdbe3441be9b298a13f
69blade69 <alexleisle@gmail.com>
2019-06-18 09:19:37 +0300

    Step 2

--- SQLStart.py
+++ SQLStart.py
@@ -1,27 +1,35 @@
 #!/usr/bin/python3
 # https://pypi.org/project/mysqlclient/ pip install mysqlclient
 import sqlalchemy
-from sqlalchemy import create_engine
+import datetime
+from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, Date, ForeignKey
+
+"""CREATE TABLE IN MYSQL"""
+metadata = MetaData()
+gen_test_table = Table('gen_test_table', metadata,
+    Column('inv_nr', Integer, primary_key=True),
+    Column('date', Date),
+    Column('code', Integer),
+    Column('Wert', Integer)
+)
+
 
 print(sqlalchemy.__version__)
 
 engine = create_engine("mysql://admin:r00t!QWE@172.22.100.200/testdb", encoding='utf-8', echo=True)
 
-""" input and sort inv_nr from testdb.uncoupling"""
+""" Connect with MySQL testdb"""
 with engine.connect() as conn:
+    metadata.create_all(engine)   # create table in MySQL
     print(conn)
 
+    """ input and sort inv_nr from testdb.uncoupling"""
     result = conn.execute(
-        #"SELECT ZVCH_INV, ZVCHDATKO, ZVKFGRL FROM testdb.wheels order by ZVCH_INV, ZVCHDATKO, ZVKFGRL limit 100000")  # conn.execute("SELECT User, Host, Password FROM mysql.user") #conn.execute("SHOW DATABASES") # conn.execute("select username from users")
-        #"SELECT * FROM testdb.wheels limit 1000")
-        #"CREATE TABLE test_table (idtest_table_id INT NOT NULL, test_tablecol VARCHAR(45) NULL, PRIMARY KEY (idtest_table_id))")
-        #"INSERT INTO test_table VALUES(001, 'XEP')")
-        #"SELECT * FROM testdb.test_table;")
         "SELECT COUNT(ZVCH_INV) AS cnt_inv, ZVCH_INV FROM testdb.uncoupling group by ZVCH_INV ORDER BY cnt_inv desc limit 20;")
 
     print('Start')
     list_inv_nr = []
-    i_inv_nr = 1
+    i_inv_nr = 0
     for row in result:
         #pass
         #print(type(row))
@@ -37,20 +45,32 @@ with engine.connect() as conn:
         # print('ROW:', row[0]) # conn.execute("SHOW DATABASES")
         # print("username:", row['User'])
     print('\n'.join(map(str, list_inv_nr)))  # построчный вывод списка
-    print(list_inv_nr[0][1])
+    # print(list_inv_nr[0][1])
 print('Ende')
 
 """read table from inv_nr with Date uncoupling """
 with engine.connect() as conn:
-    result_inv_nr = conn.execute(
-        "SELECT * FROM testdb.uncoupling WHERE ZVCH_INV = %s" % list_inv_nr[0][1])
-    list_zug = []
-    i_zug = 1
-    for row_inv_nr in result_inv_nr:
-        # print(str(row_inv_nr))
-        T_zug = (row_inv_nr)  # T = ('cc', 'aa', 'dd', 'bb')
-        tmp_zug = list(T_zug)  # Создать список из элементов кортежа
-        list_zug.append(tmp_zug)
-        i_inv_nr += 1
-    print('\n'.join(map(str, list_zug)))  # построчный вывод списка
+    for count_inv in list_inv_nr:
+        # print(count_inv[1])
+        result_inv_nr = conn.execute(
+            "SELECT ZVCH_INV, `0CALDAY`, ZVCH_BRKT FROM testdb.uncoupling WHERE ZVCH_INV = %s ORDER BY `0CALDAY` ASC" % count_inv[1])
+        list_zug = []
+        i_zug = 1
+        for row_inv_nr in result_inv_nr:
+            # print(str(row_inv_nr))
+            T_zug = (row_inv_nr)  # T = ('cc', 'aa', 'dd', 'bb')
+            tmp_zug = list(T_zug)  # Создать список из элементов кортежа
+            list_zug.append(tmp_zug)
+            i_inv_nr += 1
+        print('\n'.join(map(str, list_zug)))  # построчный вывод списка
+        # print(list_zug[0][1], list_zug[1][1])
+        i_date = 0
+        for count_date in list_zug:
+            if len(list_zug) == i_date + 1:
+                break
+            date_generated = [list_zug[i_date][1] + datetime.timedelta(days=x) for x in range(1, (list_zug[i_date+1][1] - list_zug[i_date][1]).days)]
+            i_date += 1
+            for date in date_generated:
+                print(date.strftime("%Y-%m-%d"))
+            print("----------------------------")
 #conn.close()g='utf-8', echo=True)
